badea832102bf363c16176e28d449e3cbdfa46e6
drers01 <drers01@localhost.localdomain>
2019-04-15 17:08:27 +0800

    test

--- drconf/dr.py
+++ drconf/dr.py
@@ -6,13 +6,13 @@ datefmt = '%Y-%m-%d %H:%M:%S'
 TAG='DR'
 
 #web
-PROJECT_BASE= '/home/drers01/python'
+PROJECT_BASE= '/home/drers01/python/drai'
 
 #face
 FACE_BASE='%s'%PROJECT_BASE
 FACESET = '%s/faceset'%FACE_BASE
 FACETMP = '%s/faceimg'%FACE_BASE
-
+MAX_LEARN_SELF_NUM = 30
 
 #voice
 WAV_MIN_LENGTH = 2048
