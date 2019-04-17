cd806a13dc7a8c5b46dba1869a728c76b998df44
dr <drlichang@189.cn>
2019-04-17 15:44:05 +0800

    models_2019041703

--- drers/utils.py
+++ drers/utils.py
@@ -12,10 +12,10 @@ def learnNewFace(idcode , imgpath):
 
 	_list_face_img_path = du.listFileByTime(_the_face_set_dir)
 	log.debug('the person[%s] had %d faces'%(idcode,len(_list_face_img_path)))
-	_new_img_file_index = 2
+	_new_img_file_index = 1
 	print(_list_face_img_path)
 
-	if len(_list_face_img_path) > 1 :
+	if len(_list_face_img_path) > 0 :
 		_last_img_file_name = du.getFileNameWithoutExt(_list_face_img_path[-1])
 		log.debug('faceset last face is %s'%_last_img_file_name)
 		_new_img_index = int(_last_img_file_name) + 1
