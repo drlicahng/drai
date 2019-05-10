c5989e9ab11808d96be6319034c9f3abeef85704
drers01 <drers01@localhost.localdomain>
2019-05-10 17:02:10 +0800

    zhouchunyan id

--- drconf/settings.py
+++ drconf/settings.py
@@ -100,13 +100,13 @@ TEMPLATES = [
 
 LANGUAGE_CODE = 'zh-Hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
