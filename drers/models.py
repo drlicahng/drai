d570df79ae565363eebd6c4eedf19b956f4d07ff
dr <drlichang@189.cn>
2019-05-14 15:10:47 +0800

    models

--- drers/models.py
+++ drers/models.py
@@ -10,7 +10,7 @@ class SysOrgan(models.Model):
 	class Meta:
 		db_table = 'departments'
 	id = models.AutoField(primary_key=True , db_column='id')
-	departmentId = models.OneToOneField('SysOrgan' , to_field='id')
+	parentId = models.OneToOneField('SysOrgan' )
 	name = models.CharField(max_length=60)
 	sort = models.IntegerField()
 	address = models.CharField(max_length=100)
@@ -28,7 +28,7 @@ class SysUser(models.Model):
 	
 
 	id = models.AutoField(primary_key=True , db_column='id')
-	departmentId = models.OneToOneField('SysOrgan' , to_field='id')
+	departmentId = models.OneToOneField('SysOrgan')
 	name = models.CharField(max_length=50)
 	sex = models.CharField(max_length=10)
 	login = models.CharField(max_length=50)
